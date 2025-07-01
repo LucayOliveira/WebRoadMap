15ddf5688043f56e116fd9248993353b00d3d603
LucasOliver <99622185+LucayOliveira@users.noreply.github.com>
2025-06-30 22:13:54 -0300

    Criando mais páginas
    1 - Criando mais páginas
        1.1 - Criar os arquivos html e sua estrutura
        1.2 - repetir os passos 5 e 6.1 do commit anterior

--- web/app/urls.py
+++ web/app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', homepage, name='homepage')
+    path('', homepage, name='homepage'),
+    path('loja/', loja, name='loja'),
+    path('minhaconta/', minhaconta, name='minhaconta'),
+    path('login/', login, name='login'),
+    path('carrinho/', carrinho, name='carrinho'),
+    path('checkout/', checkout, name='checkout')
 ]
--- web/app/views.py
+++ web/app/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
   return render(request,  'index.html')
+
+def loja(request):
+  return render(request,  'loja.html')
+
+def minhaconta(request):
+  return render(request,  'minhaconta.html')
+
+def login(request):
+  return render(request,  'login.html')
+
+def carrinho(request):
+  return render(request,  'carrinho.html')
+
+def checkout(request):
+  return render(request,  'checout.html')
