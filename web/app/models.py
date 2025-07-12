b3856248c2eeaed748a72b6f518c6ab3e49fe607
LucasOliver <99622185+LucayOliveira@users.noreply.github.com>
2025-07-12 15:59:44 -0300

    Criação do Super User. Add models para admin.py, ....
    1 - Criação do Super User através do comando "python  manage.py createsuperuser"
    2 - Importação dos modelos para o arquivo admin.py
    3 - Algumas alterações feitas no models.py como método __str__
    4 - instalação do pillow através da do comando "pip install pillow"
    5 - Conjunto de fotos adicionadas a pasta static

--- web/app/models.py
+++ web/app/models.py
@@ -13,16 +13,25 @@ class Cliente(models.Model):
 class Categoria(models.Model):
   nome = models.CharField(max_length=200, null=True, blank=True)
 
+  def __str__(self):
+    return self.nome
+
 class Tipo(models.Model):
   nome = models.CharField(max_length=200, null=True, blank=True)
 
+  def __str__(self):
+    return self.nome
+
 class Produto(models.Model):
-  imagem = models.CharField(max_length=400, null=True, blank=True)
+  imagem = models.ImageField(null=True, blank=True)
   nome = models.CharField(max_length=200, null=True, blank=True)
   preco = models.DecimalField(max_digits=10, decimal_places=2)
   ativo = models.BooleanField(default=True)
   categoria = models.ForeignKey(Categoria, null=True, blank=True, on_delete=models.SET_NULL)
   tipo = models.ForeignKey(Tipo, null=True, blank=True, on_delete=models.SET_NULL)
+
+  def __str__(self):
+    return f'{self.nome} - {self.preco}'
 
 class ItemEstoque(models.Model):
   cor = models.CharField(max_length=200, null=True, blank=True)
